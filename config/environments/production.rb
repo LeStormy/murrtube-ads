ded968ffe6727a07b4a243ef2fa3e4e8e4ef713b
stormy <raphael.beltrand@gmail.com>
2023-05-10 20:41:15 +0200

    asd

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,7 +81,7 @@ Rails.application.configure do
   # Use a different logger for distributed setups.
   # require "syslog/logger"
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new "app-name")
-  Rails.application.routes.default_url_options[:host] = "https://murrtube.net"
+  Rails.application.routes.default_url_options[:host] = "https://ads.murrtube.net"
 
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
